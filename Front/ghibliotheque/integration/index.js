2f7cbe444d3dd8362d928313bce0162b623fc5b4
JosephaSerain <josepha.serain@oclock.school>
2024-10-08 14:02:19 +0200

    quelques modifications sur l'aspect du site et ajout d'un select presque fonctionnel mais qui sera terminé avec la dynamisation

--- Front/ghibliotheque/integration/index.js
+++ Front/ghibliotheque/integration/index.js
@@ -6,3 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('.presentation-studio').classList.add('fade-in');
     }, 100);
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+    const typeFilter = document.getElementById('type-filter');
+    const cards = document.querySelectorAll('.card');
+  
+    typeFilter.addEventListener('change', function(){
+      const selectedType = this.value;
+  
+      cards.forEach(card => {
+        const cardTypes = card.dataset.types;
+        if (cardTypes) {
+          const typeArray = cardTypes.split(',');
+          if (selectedType === 'all' || typeArray.includes(selectedType)) {
+            card.style.display = '';
+          } else {
+            card.style.display = 'none';
+          }
+        }
+      });
+      const visibleCards = Array.from(cards).filter(card => card.style.display !== 'none');
+        visibleCards.forEach(card => cardContainer.appendChild(card));
+    });
+});
+    
